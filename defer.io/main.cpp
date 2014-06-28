a7222d0eee22e20f7da89c8ebd1454d9025f0905
신기배 <kibae@singibaeui-MacBook-Pro.local>
2014-06-29 02:13:54 +0900

    Initial Commit

---
+++ defer.io/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  defer.io
+//
+//  Created by 신기배 on 2014. 6. 29..
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
